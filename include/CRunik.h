740328dff33fb4cf8109bae7e8e874cc375afa0d
Marcos Flavio <mfghost69@gmail.com>
2025-05-01 16:50:10 -0300

    Initial commit

---
+++ include/CRunik.h
@@ -0,0 +1,4 @@
+#ifndef CRUNIK_H
+# define CRUNIK_H
+# include <stdio.h>
+#endif
---
+++ src/main.c
@@ -0,0 +1,6 @@
+#include "../include/CRunik.h"
+
+int main(void) {
+    printf("Drink up. The world’s about to end.");
+    return (0);
+}
